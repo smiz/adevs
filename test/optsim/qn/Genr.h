1beb86d077f8296222702ade20d2ce28ba38d82a
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2009-07-09 16:09:12 +0000

    Added the test case that discovered the problem with the LpGraph.
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@68 153d293f-9360-418f-87de-c6d3afdd7922

---
+++ test/optsim/qn/Genr.h
@@ -0,0 +1,41 @@
+#ifndef _Sw_h_
+#define _Sw_h_
+#include "adevs.h"
+#include <cstdlib>
+#include <iostream>
+
+class Genr: public adevs::Atomic<int>
+{
+	public:
+		Genr(int freq):adevs::Atomic<int>(),events(0),period(1.0/(double)freq)
+		{
+			srandom(1);
+		}
+		void delta_int(){ events++; }
+		void delta_ext(double, const adevs::Bag<int>&){}
+		void delta_conf(const adevs::Bag<int>&){}
+		double ta() { return period; }
+		void output_func(adevs::Bag<int>& yb) { yb.insert(random()); }
+		void gc_output(adevs::Bag<int>&){}
+		double lookahead() { return DBL_MAX; }
+	private:
+		unsigned int events;
+		const double period;
+};
+
+class Collector: public adevs::Atomic<int>
+{
+	public:
+		Collector():adevs::Atomic<int>(),events(0){}
+		void delta_int(){}
+		void delta_ext(double, const adevs::Bag<int>& xb){ events += xb.size(); }
+		void delta_conf(const adevs::Bag<int>&){}
+		double ta() { return DBL_MAX; }
+		void output_func(adevs::Bag<int>&){}
+		void gc_output(adevs::Bag<int>&){}
+		double lookahead() { return DBL_MAX; }
+	private:
+		unsigned int events;
+};
+
+#endif
