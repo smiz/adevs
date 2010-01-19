1296026d071cdea340343664445af8cbc479c0d8
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2010-01-19 21:04:08 +0000

    Documentation for the Network class
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@78 153d293f-9360-418f-87de-c6d3afdd7922

--- src/adevs_jni/adevs/Network.java
+++ src/adevs_jni/adevs/Network.java
@@ -2,7 +2,7 @@ package adevs;
 import java.util.Collection;
 
 /**
- * This is the Java interface for a DEVS network model.
+ * This is the base class for all Network (coupled) models.
  */
 public abstract class Network extends Devs
 {
@@ -11,16 +11,24 @@ public abstract class Network extends Devs
 		super(false);
 	}
 	/**
-	 * Fill the collection with the network's components.
+	 * The collection must be Fill with Network's components.
 	 * Duplicates will be ignored by the simulator. Unlike
 	 * the C++ simulator, the parent of a component is
-	 * automatically set to the model whose component set
-	 * it appears in.
+	 * automatically set to be the Network whose set of
+	 * components it appears in.
+	 *
+	 * @param	c	Empty collection to fill with the Network's components
 	 */
 	public abstract void getComponents(Collection<Devs> c);
 	/**
 	 * Route events coming into to the model or being produced
-	 * by one of its components.
+	 * by one of its components. The collection is filled with
+	 * events whose model field is the target of its value field.
+	 *
+	 * @param	x	The value of the event to be routed
+	 * @param	model	The source of the event
+	 * @param	r	An empty collection to be filled with (target,value) pairs
+	 * @see	Event
 	 */
 	public abstract void route(Object x, Devs model, Collection<Event> r);
 }
