c175c0a399801690ff705490076d60e690e41269
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2010-01-20 18:25:07 +0000

    Finished documenting the java sources and updated the Makefiles for linux
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@79 153d293f-9360-418f-87de-c6d3afdd7922

--- src/adevs_jni/adevs/EventListener.java
+++ src/adevs_jni/adevs/EventListener.java
@@ -4,19 +4,29 @@ package adevs;
  * The EventListener interface is implemented by classes
  * that will register with a Simulator to receive notification
  * when changes occur in a model's state and output.
+ *
+ * @see Simulator
  */
 public interface EventListener
 {
 	/**
 	 * This method is called when a model produces an output.
-	 * The time t is the absolute time when the output occurs
+	 * The time t is the time when the output occurs
 	 * and the Event has the model that produced the output
-	 * and the output value.
+	 * and the output's value.
+	 *
+	 * @param	x	An Event holding the model that produced the output and the output itself
+	 * @param	t	The time that the output occurred
+	 * @see	Event
 	 */
 	public void outputEvent(Event x, double t);
 	/**
 	 * This method is called immediately after an Atomic model
 	 * changes its state.
+	 *
+	 * @param	model	The model that changed state
+	 * @param	t	The time at which the change occurred
+	 * @see Atomic
 	 */
 	public void stateChange(Atomic model, double t);
 }
