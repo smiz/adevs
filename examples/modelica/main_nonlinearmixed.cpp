5d4a25b7735a2ce6c7369575556f7cc2e98cf09a
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2011-06-30 01:47:31 +0000

    Added a mixed nonlinear system to the set of examples.
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@131 153d293f-9360-418f-87de-c6d3afdd7922

---
+++ examples/modelica/main_nonlinearmixed.cpp
@@ -0,0 +1,34 @@
+#define OMC_ADEVS_IO_TYPE double
+
+#include "adevs.h"
+#include "NonlinearMixed.h"
+#include <iostream>
+
+using namespace std;
+using namespace adevs;
+
+int main()
+{
+	NonlinearMixed* nonlinearmixed = new NonlinearMixed();
+	Hybrid<OMC_ADEVS_IO_TYPE>* hybrid_model =
+		new Hybrid<OMC_ADEVS_IO_TYPE>(
+		nonlinearmixed,
+		new corrected_euler<OMC_ADEVS_IO_TYPE>(nonlinearmixed,1E-5,0.01),
+		new linear_event_locator<OMC_ADEVS_IO_TYPE>(nonlinearmixed,1E-5));
+        // Create the simulator
+        Simulator<OMC_ADEVS_IO_TYPE>* sim =
+			new Simulator<OMC_ADEVS_IO_TYPE>(hybrid_model);
+		cout << "# time, xx, x, y, z" << endl;
+        while (sim->nextEventTime() <= 5.0)
+		{
+			cout << sim->nextEventTime() << " ";
+			sim->execNextEvent();
+			cout << nonlinearmixed->get_$Pxx() << " "  
+				<< nonlinearmixed->get_$Px() << " "
+				<< nonlinearmixed->get_$Py() << " "
+				<< nonlinearmixed->get_$Pz() << endl;
+		}
+        delete sim;
+		delete hybrid_model;
+        return 0;
+}
