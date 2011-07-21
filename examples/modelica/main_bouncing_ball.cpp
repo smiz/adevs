1bf4aa96fe833e84aa513fc026ba7bab8a24d1a5
James Nutaro <nutarojj@ornl.gov>
2011-07-21 12:54:19 +0000

    Added supporting files for the bouncing ball example

---
+++ examples/modelica/main_bouncing_ball.cpp
@@ -0,0 +1,32 @@
+#include "adevs.h"
+#include "BouncingBall.h"
+#include <iostream>
+
+using namespace std;
+using namespace adevs;
+
+int main()
+{
+	BouncingBall* ball = new BouncingBall();
+	ball->setEventHysteresis(1E-6);
+	Hybrid<OMC_ADEVS_IO_TYPE>* hybrid_model =
+		new Hybrid<OMC_ADEVS_IO_TYPE>(
+		ball,
+		new corrected_euler<OMC_ADEVS_IO_TYPE>(ball,1E-8,0.01),
+		new linear_event_locator<OMC_ADEVS_IO_TYPE>(ball,1E-8));
+        // Create the simulator
+        Simulator<OMC_ADEVS_IO_TYPE>* sim =
+			new Simulator<OMC_ADEVS_IO_TYPE>(hybrid_model);
+		cout << "# time, h, v, flying" << endl;
+        while (sim->nextEventTime() <= 1.0)
+		{
+			cout << sim->nextEventTime() << " ";
+			sim->execNextEvent();
+			cout << ball->get_$Ph() << " " <<
+				ball->get_$Pv() << " " <<
+				ball->get_$Pflying() << endl;
+		}
+        delete sim;
+		delete hybrid_model;
+        return 0;
+}
