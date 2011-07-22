c470b56ff611a7eead2feed8c99654636646e8dc
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2011-07-22 19:45:17 +0000

    Fixed up the modelica runtime environment to solve linking problems with omc's C runtime environment
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@139 153d293f-9360-418f-87de-c6d3afdd7922

---
+++ include/adevs_modelica_runtime.h
@@ -0,0 +1,22 @@
+#ifndef _ADEVS_SIMULATION_RUNTIME_H
+#define _ADEVS_SIMULATION_RUNTIME_H
+#include "simulation_varinfo.h"
+
+extern int LOG_STATS;
+extern int LOG_INIT;
+extern int LOG_RES_INIT;
+extern int LOG_SOLVER;
+extern int LOG_EVENTS;
+extern int LOG_NONLIN_SYS;
+extern int LOG_ZEROCROSSINGS;
+extern int LOG_DEBUG;
+extern int sim_verbose;
+extern int modelErrorCode;
+extern int ERROR_NONLINSYS;
+extern int ERROR_LINSYS;
+
+void MODELICA_ASSERT(omc_fileInfo info, const char* msg);
+void MODELICA_TERMINATE(const char* msg);
+
+#endif
+
