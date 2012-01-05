dab6bdb7b5297a0ee1eae9bd82f8646da2fc16fb
smiz <smiz@153d293f-9360-418f-87de-c6d3afdd7922>
2012-01-05 00:02:19 +0000

    Added script for getting and building OpenModelica
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@155 153d293f-9360-418f-87de-c6d3afdd7922

---
+++ util/build-omc.sh
@@ -0,0 +1,32 @@
+# This shell script will download and built the OpenModelica compiler
+# and object files that are required to use Modelica models with
+# adevs. If you run this script in the director D then this script
+# will place the modelica compiler into D/trunk/build/bin and
+# you should point MODELICA_HOME in the adevs Makefile to D/trunk. 
+mkdir openmodelica
+cd openmodelica
+# Build smlnj and put it in our path
+mkdir smlnj
+cd smlnj
+wget http://smlnj.cs.uchicago.edu/dist/working/110.73/config.tgz
+tar xfvz config.tgz
+config/install.sh
+export SMLNJ_HOME=$PWD
+export PATH=$PWD/bin:$PATH
+cd ..
+# Build the RML compiler and put it in our path
+mkdir rml
+svn co https://openmodelica.org/svn/MetaModelica/trunk mmc
+RMLHOME=$PWD/rml
+cd mmc
+./configure --prefix=$RMLHOME
+make
+make install
+cd ..
+export PATH=$RMLHOME/bin:$PATH
+# Build the openmodelica compiler
+svn co https://openmodelica.org/svn/OpenModelica/trunk/
+cd trunk
+./configure --without-paradiseo --enable-omterminal-shell=no
+make omc
+# Done
