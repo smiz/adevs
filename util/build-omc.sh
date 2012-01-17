e6ecadfe3780393a19c971b70a12cc960deaeab6
James Nutaro <nutarojj@ornl.gov>
2012-01-17 01:18:35 +0000

    Added to build-omc a test for the configure file

--- util/build-omc.sh
+++ util/build-omc.sh
@@ -3,8 +3,9 @@
 # adevs. If you run this script in the director D then this script
 # will place the modelica compiler into D/trunk/build/bin and
 # you should point MODELICA_HOME in the adevs Makefile to D/trunk. 
+HOMEDIR=$PWD/openmodelica
 mkdir openmodelica
-cd openmodelica
+cd $HOMEDIR
 # Build smlnj and put it in our path
 mkdir smlnj
 cd smlnj
@@ -13,20 +14,26 @@ tar xfvz config.tgz
 config/install.sh
 export SMLNJ_HOME=$PWD
 export PATH=$PWD/bin:$PATH
-cd ..
+cd $HOMEDIR
 # Build the RML compiler and put it in our path
 mkdir rml
 svn co --username=anonymous --password=none https://openmodelica.org/svn/MetaModelica/trunk mmc
-RMLHOME=$PWD/rml
+export RMLHOME=$PWD/rml
 cd mmc
 ./configure --prefix=$RMLHOME
 make
 make install
-cd ..
+cd $HOMEDIR
 export PATH=$RMLHOME/bin:$PATH
 # Build the openmodelica compiler
 svn co https://openmodelica.org/svn/OpenModelica/trunk/
 cd trunk
+# Check for the configure file. If its not there, then create it.
+if [ ! -x configure ];
+then
+	autoconf
+fi
+# Configure and build omc
 ./configure --without-paradiseo --enable-omshell-terminal=no
 make omc
 # Done
