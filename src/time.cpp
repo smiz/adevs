cbd8444ed2521c8c46936636908de5f9cb02af30
ssuryaputra <ssuryaputra@153d293f-9360-418f-87de-c6d3afdd7922>
2012-04-23 22:20:22 +0000

    Add double_fcmp_test.cpp and modify time.cpp to use Time<> instead of Time for the default basic type of class Time.
    git-svn-id: http://svn.code.sf.net/p/adevs/code/trunk@165 153d293f-9360-418f-87de-c6d3afdd7922

--- src/time.cpp
+++ src/time.cpp
@@ -1,6 +1,6 @@
 #include "adevs_time.h"
 
-std::ostream& operator<<(std::ostream& strm, const adevs::Time& t)
+std::ostream& operator<<(std::ostream& strm, const adevs::Time<>& t)
 {
 	strm << "(" << t.t << "," << t.c << ")";
 	return strm;
