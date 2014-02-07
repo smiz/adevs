e6e76cf51cb98da286ab5f07cd14a77899587996
James Nutaro <nutarojj@ornl.gov>
2014-02-07 17:34:36 +0000

    Added test for uniform distribution

--- test/rv_test.cpp
+++ test/rv_test.cpp
@@ -6,6 +6,32 @@
 using namespace std;
 using namespace adevs;
 
+void test_uniform()
+{
+	int bins[101];
+	for (int i = 0; i < 101; i++)
+		bins[i] = 0;
+	double a = 0.0, b = 1.0;
+	double sum = 0.0;
+	int count = 100000000;
+	rv r(getpid());
+	for (int i = 0; i < count; i++)
+	{
+		double s = r.uniform(a,b);
+		assert(s >= a && s <= b);
+		sum += s;
+		bins[int(100.0*s)]++;
+	}
+	for (int i = 1; i < 100; i++)
+	{
+		int diff = abs(bins[i] - bins[i-1]);
+		if (diff >= 10000) cout << i << " " << diff << " " << 
+			((double)(diff)/(double)(count)) << endl;
+		assert(diff < 10000);
+	}
+	assert(fabs((a+b)/2.0 - sum/(double)(count)) < 1E-2);
+}
+
 void test_triangular()
 {
 	int bins[101];
@@ -35,5 +61,6 @@ void test_triangular()
 int main()
 {
 	test_triangular();
+	test_uniform();
 	return 0;
 }
