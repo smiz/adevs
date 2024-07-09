afd224669e397d66de018ee0ff0785b13d16cf3b
Seth Hitefield <hitefieldsd@ornl.gov>
2024-07-09 16:27:42 -0400

    Adding tests to the meson build

---
+++ test/helpers/run_and_compare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Executes the test and compares the output to a known good output. Since line
+# order may change and whitespace does not matter, first sort all lines of the
+# output before passing it to diff and also ignore whitespace.
+
+echo "Running '$1' with input '$2' and comparing to '$3'"
+$1 < $2 | sort | diff --ignore-space-change --ignore-blank-lines <(sort $3) -
