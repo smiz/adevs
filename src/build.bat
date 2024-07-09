7f358b964050aeded255e68a11d035a3453f5b6f
Seth Hitefield <hitefieldsd@ornl.gov>
2024-07-09 16:27:39 -0400

    Removing java bindings
    We can bring this back if we really need it.

--- src/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cl /nologo /O2 /GR /MT /EHsc /I..\include /c poly.cpp 
-cl /nologo /O2 /GR /MT /EHsc /I..\include /c rv.cpp 
-lib /VERBOSE /OUT:adevs.lib *.obj
-del *.obj
-echo Built static library adevs.lib
