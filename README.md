7b7d9f3f22c132df58da9c3600538efa58ba2326
Jim Nutaro <nutaro@gmail.com>
2025-07-24 18:56:58 -0400

    Add simple make build system for people who just want to use the library. Updated README with build instructions. Move fcmp function into the src directory with adevs.cpp

---
+++ README.md
@@ -0,0 +1,36 @@
+# A Discrete EVent system Simulator (adevs) 
+
+This is an implement of the DEVS modeling and simulation
+formalism. For a quick start run
+
+'make'
+
+This will build a static link library libadevs.a and the
+HTML documentation. The documentation appears in the
+html directory and you can get started with it by opening
+the file html/index.html with your browser.
+
+If you just want the library use
+
+'make lib'
+
+If you just want the documentation use
+
+'make docs'
+
+To cleanup the library and docs use
+
+'make clean'
+
+## Testing
+
+If you want to run the tests you will need to install meson.
+You will also need the [SUNDIALS](https://computing.llnl.gov/projects/sundials)
+development libraries. To build and run the tests use
+
+'meson setup builddir'
+'meson compile -C builddir'
+'meson test -C builddir'
+
+
+
--- include/adevs/time.h
+++ include/adevs/time.h
@@ -162,67 +162,6 @@ class sd_time {
     int k;
 };
 
-/**
- * @brief A double value where equality is defined within a given tolerance.
- *
- * The fcmp() inline function is taken from fcmp.c, which is
- * Copyright (c) 1998-2000 Theodore C. Belding,
- * University of Michigan Center for the Study of Complex Systems,
- * <mailto:Ted.Belding@umich.edu>,
- * <http://www-personal.umich.edu/~streak/>,
- *
- * This code is part of the fcmp distribution. fcmp is free software;
- * you can redistribute and modify it under the terms of the GNU Library
- * General Public License (LGPL), version 2 or later.  This software
- * comes with absolutely no warranty.
- * 
- * @param x1 First double value to compare
- * @param x2 Second double value to compare
- * @param epsilon The tolerance for the comparison
- * @return  1 if x1 > x2, -1 if x1 < x2, and 0 if they are equal within the tolerance
- */
-inline int fcmp(double x1, double x2, double epsilon) {
-    int exponent;
-    double delta;
-    double difference;
-
-    /* Get exponent(max(fabs(x1), fabs(x2))) and store it in exponent. */
-
-    /* If neither x1 nor x2 is 0, */
-    /* this is equivalent to max(exponent(x1), exponent(x2)). */
-
-    /* If either x1 or x2 is 0, its exponent returned by frexp would be 0, */
-    /* which is much larger than the exponents of numbers close to 0 in */
-    /* magnitude. But the exponent of 0 should be less than any number */
-    /* whose magnitude is greater than 0. */
-
-    /* So we only want to set exponent to 0 if both x1 and */
-    /* x2 are 0. Hence, the following works for all x1 and x2. */
-
-    frexp(fabs(x1) > fabs(x2) ? x1 : x2, &exponent);
-
-    /* Do the comparison. */
-
-    /* delta = epsilon * pow(2, exponent) */
-
-    /* Form a neighborhood around x2 of size delta in either direction. */
-    /* If x1 is within this delta neighborhood of x2, x1 == x2. */
-    /* Otherwise x1 > x2 or x1 < x2, depending on which side of */
-    /* the neighborhood x1 is on. */
-
-    delta = ldexp(epsilon, exponent);
-
-    difference = x1 - x2;
-
-    if (difference > delta) {
-        return 1; /* x1 > x2 */
-    } else if (difference < -delta) {
-        return -1; /* x1 < x2 */
-    } else {       /* -delta <= difference <= delta */
-        return 0;  /* x1 == x2 */
-    }
-}
-
 /** 
  * @brief This is an alternative double that may be used for the simulation clock
  * (i.e., as the template parameter TimeType for models and simulators).
@@ -309,6 +248,27 @@ class double_fcmp {
         d -= t2.d;
         return *this;
     }
+
+    /**
+     * @brief A double value where equality is defined within a given tolerance.
+     *
+     * The fcmp() inline function is taken from fcmp.c, which is
+     * Copyright (c) 1998-2000 Theodore C. Belding,
+     * University of Michigan Center for the Study of Complex Systems,
+     * <mailto:Ted.Belding@umich.edu>,
+     * <http://www-personal.umich.edu/~streak/>,
+     *
+     * This code is part of the fcmp distribution. fcmp is free software;
+     * you can redistribute and modify it under the terms of the GNU Library
+     * General Public License (LGPL), version 2 or later.  This software
+     * comes with absolutely no warranty. The implement is in adevs.cpp
+     * 
+     * @param x1 First double value to compare
+     * @param x2 Second double value to compare
+     * @param epsilon The tolerance for the comparison
+     * @return  1 if x1 > x2, -1 if x1 < x2, and 0 if they are equal within the tolerance
+     */
+    static int fcmp(double x1, double x2, double epsilon);
 };
 
 }  // namespace adevs
